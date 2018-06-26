d11e2b409a9db1f13484214e0d951b396a5fb690
Kimeld <amed.doukoure@hetic.net>
2018-06-26 15:49:09 +0200

    police add, home add

---
+++ scripts/scriptHome.js
@@ -0,0 +1,12 @@
+let slideButton = document.querySelector(".slideButton")
+let circle = document.querySelector(".circle")
+
+
+slideButton.addEventListener('click', () => {
+    console.log(slideButton)
+    circle.classList.add('transitionUp');
+
+
+
+
+})
