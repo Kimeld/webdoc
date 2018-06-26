f6246d11f0d24423e43ae96755e7aa9b69f13b7a
Kimeld <amed.doukoure@hetic.net>
2018-06-26 17:14:58 +0200

    fin home

--- scripts/scriptHome.js
+++ scripts/scriptHome.js
@@ -1,10 +1,12 @@
 let slideButton = document.querySelector(".slideButton")
 let circle = document.querySelector(".circle")
+let introText = document.querySelector(".introText")
 
 
 slideButton.addEventListener('click', () => {
     console.log(slideButton)
     circle.classList.add('transitionUp');
+    introText.classList.add('transitionOpacity');
 
 
 
