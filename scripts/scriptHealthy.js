910c5d65e1441aaf6b71de1b2180984fb2c5a8c4
Kimeld <amed.doukoure@hetic.net>
2018-06-26 23:31:00 +0200

    temoinage

---
+++ scripts/scriptHealthy.js
@@ -0,0 +1,28 @@
+let person1 = document.querySelector('.person1')
+let person2 = document.querySelector('.person2')
+let text1 = document.querySelector('.text1')
+
+
+person1.addEventListener('click', () => {
+    console.log(person1)
+    text1.innerHTML = "“Manger sain, c’est toujours synonyme de privation et de frustration. Mais on se rend vite compte que l’on ne s’est jamais autant amusé avec la nourriture ! Je mange varié et je découvre de nouvelles saveurs. Bref, on s’éclate nous, les healthy vegan !” _ Gala";
+    text1.classList.remove('opacity');
+
+
+
+
+
+})
+
+
+person2.addEventListener('click', () => {
+    console.log(person1)
+    text1.innerHTML = "“ c’est toujours synonyme de privation et de frustration. Mais on se rend vite compte que l’on ne s’est jamais autant amusé avec la nourriture ! Je mange varié et je découvre de nouvelles saveurs. Bref, on s’éclate nous, les healthy vegan !” _ Gala";
+    text1.classList.remove('opacity');
+
+
+
+
+
+
+})
