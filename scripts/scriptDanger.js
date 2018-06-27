a6515971dbeaf0b8062fc3a78d19e310b4836ef4
Kimeld <amed.doukoure@hetic.net>
2018-06-27 15:39:55 +0200

    slide danger

--- scripts/scriptDanger.js
+++ scripts/scriptDanger.js
@@ -6,3 +6,28 @@ let containerHeight = document.querySelector('height')
 let height = windowHeight
 console.log(chimic)
 chimic.style.height = windowHeight
+
+let text = document.querySelector(".text ")
+let defoImg = document.querySelector(".defoImg")
+let circlePopup2 = document.querySelector(".circlePopup2")
+let circlePopup = document.querySelector(".circlePopup")
+
+circlePopup.addEventListener('click', () => {
+    console.log(circlePopup)
+    text.classList.add('moveDownText');
+    defoImg.classList.add('moveUpImage');
+
+
+
+
+})
+
+circlePopup2.addEventListener('click', () => {
+    console.log(circlePopup2)
+    text.classList.remove('moveDownText');
+    defoImg.classList.remove('moveUpImage');
+
+
+
+
+})
