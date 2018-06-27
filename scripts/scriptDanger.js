2547c6cb8023b608f1ba24e739abf18d946b5727
Kimeld <amed.doukoure@hetic.net>
2018-06-27 22:29:29 +0200

    fin JS slide

--- scripts/scriptDanger.js
+++ scripts/scriptDanger.js
@@ -13,6 +13,10 @@ let defoImg = document.querySelector(".defoImg")
 let text2 = document.querySelector(".text2 ")
 let defoImg2 = document.querySelector(".defoImg2")
 
+let circlePopup8 = document.querySelector(".circlePopup8")
+let circlePopup7 = document.querySelector(".circlePopup7")
+let circlePopup6 = document.querySelector(".circlePopup6")
+let circlePopup5 = document.querySelector(".circlePopup5")
 let circlePopup4 = document.querySelector(".circlePopup4")
 let circlePopup3 = document.querySelector(".circlePopup3")
 let circlePopup2 = document.querySelector(".circlePopup2")
@@ -75,6 +79,77 @@ circlePopup4.addEventListener('click', () => {
     defoImg4.classList.add('moveUpImage2');
     circlePopup4.classList.add('opacityButton');
     circlePopup3.classList.remove('opacityButton');
+
+
+})
+
+
+//3 sur 4
+
+let text5 = document.querySelector(".text5 ")
+let defoImg5 = document.querySelector(".defoImg5")
+
+let text6 = document.querySelector(".text6 ")
+let defoImg6 = document.querySelector(".defoImg6")
+
+
+circlePopup5.addEventListener('click', () => {
+    console.log(circlePopup)
+    text5.classList.add('moveDownText');
+    defoImg5.classList.add('moveUpImage');
+    text6.classList.remove('moveDownText2');
+    defoImg6.classList.remove('moveUpImage2');
+    circlePopup5.classList.add('opacityButton');
+    circlePopup6.classList.remove('opacityButton');
+
+
+
+
+})
+
+circlePopup6.addEventListener('click', () => {
+    console.log(circlePopup2)
+    text5.classList.remove('moveDownText');
+    defoImg5.classList.remove('moveUpImage');
+    text6.classList.add('moveDownText2');
+    defoImg6.classList.add('moveUpImage2');
+    circlePopup6.classList.add('opacityButton');
+    circlePopup5.classList.remove('opacityButton');
+
+
+})
+
+//4 sur 4
+
+let text7 = document.querySelector(".text7 ")
+let defoImg7 = document.querySelector(".defoImg7")
+
+let text8 = document.querySelector(".text8 ")
+let defoImg8 = document.querySelector(".defoImg8")
+
+
+circlePopup7.addEventListener('click', () => {
+    console.log(circlePopup)
+    text7.classList.add('moveDownText');
+    defoImg7.classList.add('moveUpImage');
+    text8.classList.remove('moveDownText2');
+    defoImg8.classList.remove('moveUpImage2');
+    circlePopup7.classList.add('opacityButton');
+    circlePopup8.classList.remove('opacityButton');
+
+
+
+
+})
+
+circlePopup8.addEventListener('click', () => {
+    console.log(circlePopup2)
+    text7.classList.remove('moveDownText');
+    defoImg7.classList.remove('moveUpImage');
+    text8.classList.add('moveDownText2');
+    defoImg8.classList.add('moveUpImage2');
+    circlePopup8.classList.add('opacityButton');
+    circlePopup7.classList.remove('opacityButton');
 
 
 })
