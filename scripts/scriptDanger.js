d5ef7c4a46fa96dbf083f24b761917039a7cc62c
Kimeld <amed.doukoure@hetic.net>
2018-06-27 12:18:01 +0200

    eco/danger

---
+++ scripts/scriptDanger.js
@@ -0,0 +1,8 @@
+let windowWidth = window.innerWidth
+let windowHeight = window.innerHeight
+let chimic = document.querySelector('.chimic')
+let containerHeight = document.querySelector('height')
+
+let height = windowHeight
+console.log(chimic)
+chimic.style.height = windowHeight
