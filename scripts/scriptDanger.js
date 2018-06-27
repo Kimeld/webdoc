b04409628f155a5208f969e04d65de258e41aa45
Kimeld <amed.doukoure@hetic.net>
2018-06-27 16:04:44 +0200

    fin premier slider

--- scripts/scriptDanger.js
+++ scripts/scriptDanger.js
@@ -9,6 +9,10 @@ chimic.style.height = windowHeight
 
 let text = document.querySelector(".text ")
 let defoImg = document.querySelector(".defoImg")
+
+let text2 = document.querySelector(".text2 ")
+let defoImg2 = document.querySelector(".defoImg2")
+
 let circlePopup2 = document.querySelector(".circlePopup2")
 let circlePopup = document.querySelector(".circlePopup")
 
@@ -16,6 +20,8 @@ circlePopup.addEventListener('click', () => {
     console.log(circlePopup)
     text.classList.add('moveDownText');
     defoImg.classList.add('moveUpImage');
+    text2.classList.remove('moveDownText2');
+    defoImg2.classList.remove('moveUpImage2');
 
 
 
@@ -26,6 +32,8 @@ circlePopup2.addEventListener('click', () => {
     console.log(circlePopup2)
     text.classList.remove('moveDownText');
     defoImg.classList.remove('moveUpImage');
+    text2.classList.add('moveDownText2');
+    defoImg2.classList.add('moveUpImage2');
 
 
 
